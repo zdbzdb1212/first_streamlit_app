3d117a645c82de784b1313851504b075dbe7c5f9
zdbzdb1212 <70541464+zdbzdb1212@users.noreply.github.com>
2022-12-13 11:18:26 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,15 +21,14 @@ fruits = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits)
 
 streamlit.header('Fruityvice Fruit Advice!')
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-
-# Put that shit in a table
-fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
-# output table as dataframe
-streamlit.dataframe(fruityvice_normalized)
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+  if not fruit_choice:
+      streamlit.error("Please select a fruit to get information.")
+  else
+      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+      fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
+      streamlit.dataframe(fruityvice_normalized)
 
 streamlit.stop()
 
